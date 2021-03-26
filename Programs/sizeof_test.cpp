2b2c67b09d320bc3ef1ca2b575bb0246f56e2155
Jerin John Mathew <71806917+J3r1n-sudo@users.noreply.github.com>
2021-03-26 23:33:38 +0530

    Create sizeof_test.cpp

---
+++ Programs/sizeof_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "int size ="<<sizeof(int)<<endl;
+    cout << "short size ="<<sizeof(short)<<endl;
+    cout << "int long ="<<sizeof(long)<<endl;
+    cout << "int char ="<<sizeof(char)<<endl;
+    cout << "int float ="<<sizeof(float)<<endl;
+    cout << "int double ="<<sizeof(double)<<endl;
+    cout << "int bool ="<<sizeof(bool)<<endl;
+}
+
+
